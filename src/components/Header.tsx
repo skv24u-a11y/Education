1a1bfc66d0b71644c5ab04f1fc29eff30c88d674
skv24u-a11y <skv24u@gmail.com>
2025-08-22 19:07:14 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC = () => {
   <img
     src="https://i.ibb.co/393rK7nD/generated-image.png"
     alt="Matipa College of Nursing Logo"
-    className="w-16 h-20 object-contain rounded-lg shadow-lg"
+    className="w-25 h-30 object-contain rounded-lg shadow-lg"
   />
   <div>
     <h1 className="text-2xl font-bold bg-gradient-to-r from-orange-600 to-red-600 bg-clip-text text-transparent">
